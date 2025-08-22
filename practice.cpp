2420caa1f8cc154cfd85e2e3f3b3156c32302a82
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-22 20:08:37 +0530

    Learning OOPs Concepts(Tyoes of Constructor)

--- practice.cpp
+++ practice.cpp
@@ -41,22 +41,79 @@
 
 // Encapsulation is wrapping up of data and member function in a single unit called class.
 
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// class Account{
+// private:
+//     string balance;  // Data Hiding
+//     string passsword;    
+
+// public:    
+//     string accountId;
+//     string username;
+
+// };
+
+// int main(){
+
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<string>
 using namespace std;
 
-class Account{
-private:
-    string balance;  // Data Hiding
-    string passsword;    
+class Teacher{
+    private:
+    double salary;
 
-public:    
-    string accountId;
-    string username;
+    public:
+    string name;
+    string dept;
+    string subject;
 
+    // non-parameterized constructor
+    Teacher(){
+        dept = "Computer Science";
+    }
+
+    // Paramerterized Constructor
+    Teacher(string name, string dept, string subject, double salary){
+        this->name = name;
+        this->dept = dept;
+        this->subject = subject;
+        this->salary = salary;
+    }
+    // Countructor Overloading : More than one constructor (Also an example of Polymorphism).
+
+    // Copy Constructor
+    Teacher(Teacher &orjObject){
+        cout<<"I am a custom copy constructor."<<endl;
+        this->name = orjObject.name;
+        this->dept = orjObject.dept;
+        this->subject = orjObject.subject;
+        this->salary = orjObject.salary;
+    }
+
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+
+    void getInfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Department : "<<dept<<endl;
+        cout<<"Subject : "<<subject<<endl;
+        cout<<"Salary : "<<salary<<endl;
+    }
 };
 
 int main(){
-
+    Teacher t1("Mihran", "Computer Science", "C++", 2500);
+    // t1.getInfo();
+    Teacher t2(t1); // Default Copy Constructor-invoke
+    t2.getInfo();
     return 0;
 }
